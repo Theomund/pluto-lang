7ca8ff63a6ed4c35d7bc91130a6ed169fe253efc
Theomund <gregoryshuck96@gmail.com>
2019-04-24 17:37:48 -0400

    Added LLVM code generation for selection statements

--- src/Codegen.hs
+++ src/Codegen.hs
@@ -9,7 +9,6 @@ import Control.Lens
 import Control.Lens.Prism
 import Control.Monad
 import Data.ByteString.Char8 as BS (putStrLn)
-import Data.Either
 import qualified Data.Map.Strict as Map
 import LLVM.AST hiding (Add, Call, Div, Mul, Sub, element, function)
 import qualified LLVM.AST.Constant as C
@@ -40,21 +39,41 @@ generateModule xs =
 codegenDecl :: MonadModuleBuilder m => Decl -> m Operand
 codegenDecl (Var a (Constant b)) = global (mkName a) AST.i32 (C.Int 32 b)
 codegenDecl (Extern a b) =
-  extern (mkName a) (toListOf (replicated $ length b) AST.i32) AST.i32
+  extern (mkName a) [] AST.i32
 codegenDecl (Func a b (CompoundStmt c)) =
   function (mkName a) [] AST.i32 $ \[] ->
-    mdo forOf each c $ \case
+    mdo _entry <- block `named` "entry"
+        forOf each c $ \case
           StmtItem v -> codegenStmt v
           DeclItem v -> error "Local variables are not implemented yet."
         ret (ConstantOperand (C.Int 32 0))
 
 codegenStmt :: MonadIRBuilder m => Stmt -> m Operand
 codegenStmt (ExprStmt a) = codegenExpr a
+codegenStmt (If a b) = do
+  cond <- codegenExpr a
+  condBr cond "if.then" "if.exit"
+  ifThen <- block `named` "if.then"
+  trVal <- codegenStmt b
+  br "if.exit"
+  ifExit <- block `named` "if.exit"
+  phi [(trVal, ifThen)]
+codegenStmt (IfElse a b c) = do
+  cond <- codegenExpr a
+  condBr cond "if.then" "if.else"
+  ifThen <- block `named` "if.then"
+  trVal <- codegenStmt b
+  br "if.exit"
+  ifElse <- block `named` "if.else"
+  flVal <- codegenStmt c
+  br "if.exit"
+  ifExit <- block `named` "if.exit"
+  phi [(trVal, ifThen), (flVal, ifElse)]
 
 codegenExpr :: MonadIRBuilder m => Expr -> m Operand
 codegenExpr (Constant a) = return $ ConstantOperand $ C.Int 32 a
 codegenExpr (Call a b) =
-  call (ConstantOperand (C.GlobalReference AST.i32 (mkName a))) []
+  call (ConstantOperand (C.GlobalReference AST.i32 (mkName "a"))) []
 codegenExpr (Binary op a b) =
   mdo let binOps =
             Map.fromList
